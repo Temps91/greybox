9d353495bedbdc210e68ce9dff6ff8d09dc7a1b0
Temps91 <jorgebd3045@gmail.com>
2025-10-06 01:03:49 -0600

    Cambios a audio Manager de bichos

--- Assets/AssetsProject/Scripts/BarreraScripts/AudioManager.cs
+++ Assets/AssetsProject/Scripts/BarreraScripts/AudioManager.cs
@@ -3,9 +3,14 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     public AudioClip caida;
+    public AudioClip bichos;
 
     public void CaidaSound(Vector3 posicion)
     {
         AudioSource.PlayClipAtPoint(caida, posicion);
+    }
+    public void BichoSound(Vector3 posicion)
+    {
+        AudioSource.PlayClipAtPoint(bichos, posicion);
     }
 }
