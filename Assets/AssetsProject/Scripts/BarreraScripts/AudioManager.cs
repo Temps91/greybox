3af6a457fec0a17df1927b5b1db086aee22c312a
Temps91 <jorgebd3045@gmail.com>
2025-10-09 12:51:11 -0600

    Subiendo Script de sonidos

--- Assets/AssetsProject/Scripts/BarreraScripts/AudioManager.cs
+++ Assets/AssetsProject/Scripts/BarreraScripts/AudioManager.cs
@@ -1,17 +1,92 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    public static AudioManager Instance; 
+
+    [Header("Sonidos individuales")]
     public AudioClip caida;
     public AudioClip bichos;
 
+    [Header("Colecciones de sonidos")]
+    public AudioClip[] sonidosInofensivos;
+    public AudioClip[] sonidosDeSusto;
+    public AudioClip[] dialogos;
+    public AudioClip[] ambientes;
+
+    private AudioSource ambienteSource; 
+    private AudioSource dialogoSource;  
+
+    private void Awake()
+    {
+
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); 
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+
+        ambienteSource = gameObject.AddComponent<AudioSource>();
+        ambienteSource.loop = true;
+
+        dialogoSource = gameObject.AddComponent<AudioSource>();
+    }
+
     public void CaidaSound(Vector3 posicion)
     {
-        AudioSource.PlayClipAtPoint(caida, posicion);
-        Debug.Log("Sonando caida");
+        if (caida != null)
+            AudioSource.PlayClipAtPoint(caida, posicion);
     }
+
+
     public void BichoSound(Vector3 posicion)
     {
-        AudioSource.PlayClipAtPoint(bichos, posicion);
+        if (bichos != null)
+            AudioSource.PlayClipAtPoint(bichos, posicion);
+    }
+
+
+    public void PlayInofensivo(Vector3 posicion)
+    {
+        if (sonidosInofensivos.Length > 0)
+        {
+            AudioClip clip = sonidosInofensivos[Random.Range(0, sonidosInofensivos.Length)];
+            AudioSource.PlayClipAtPoint(clip, posicion);
+        }
+    }
+
+
+    public void PlaySusto(Vector3 posicion)
+    {
+        if (sonidosDeSusto.Length > 0)
+        {
+            AudioClip clip = sonidosDeSusto[Random.Range(0, sonidosDeSusto.Length)];
+            AudioSource.PlayClipAtPoint(clip, posicion);
+        }
+    }
+
+    public void PlayDialogo(int index)
+    {
+        if (dialogos.Length > index && dialogos[index] != null)
+        {
+            dialogoSource.Stop();
+            dialogoSource.clip = dialogos[index];
+            dialogoSource.Play();
+        }
+    }
+
+    public void CambiarAmbiente(int index)
+    {
+        if (ambientes.Length > index && ambientes[index] != null)
+        {
+            ambienteSource.clip = ambientes[index];
+            ambienteSource.Play();
+        }
     }
 }
---
+++ Assets/AssetsProject/Scripts/BarreraScripts/SustosInofensivos.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class SustosInofensivos : MonoBehaviour
+{
+    public enum TipoDeSonido
+    {
+        Inofensivo,
+        Susto,
+        Dialogo,
+        Ambiente
+    }
+
+    [Header("Zona de sonido")]
+    public TipoDeSonido tipo;
+    public GameObject jugador;       
+    public Transform puntoDeSonido;  
+    public int indexDialogo = 0;     
+    private bool activado = false;   
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (activado) return;
+
+        if (other.gameObject == jugador)
+        {
+            activado = true;
+            Vector3 posicion = puntoDeSonido != null ? puntoDeSonido.position : transform.position;
+
+            switch (tipo)
+            {
+                case TipoDeSonido.Inofensivo:
+                    AudioManager.Instance.PlayInofensivo(posicion);
+                    break;
+
+                case TipoDeSonido.Susto:
+                    AudioManager.Instance.PlaySusto(posicion);
+                    break;
+
+                case TipoDeSonido.Dialogo:
+                    AudioManager.Instance.PlayDialogo(indexDialogo);
+                    break;
+
+                case TipoDeSonido.Ambiente:
+                    AudioManager.Instance.CambiarAmbiente(indexDialogo);
+                    break;
+            }
+        }
+    }
+
+}
