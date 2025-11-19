49a6e3a2a262ba3408fa0cbf4fa4b8a7255d87a6
SonocJM <sonxzisulishimada@gmail.com>
2025-11-19 13:42:24 -0600

    adding more things in cauldron

--- Assets/AssetsProject/Scripts/SonocScripts/DemonsManager.cs
+++ Assets/AssetsProject/Scripts/SonocScripts/DemonsManager.cs
@@ -1,8 +1,15 @@
+using System;
 using UnityEngine;
 
 public class DemonsManager : MonoBehaviour
 {
     public Demons[] allDemons;
+    public GameObject[] enemyGenericItems;
+
+    private void Start()
+    {
+        DesactivateAllEnemyItems();
+    }
 
     public void CheckItem(Item pickedItem)
     {
@@ -12,8 +19,25 @@ public class DemonsManager : MonoBehaviour
             if (activated)
             {
                 demon.ActiveAllObjects();
+                ActiveAllEnemyItems();
                 Debug.Log($"Evento: Demon {demon.demonName} activado por el manager");
             }
+        }
+    }
+    
+    public void ActiveAllEnemyItems()
+    {
+        for(int i = 0; i < enemyGenericItems.Length; i++)
+        {
+            enemyGenericItems[i].SetActive(true);
+        }
+    }
+    
+    public void DesactivateAllEnemyItems()
+    {
+        for(int i = 0; i < enemyGenericItems.Length; i++)
+        {
+            enemyGenericItems[i].SetActive(false);
         }
     }
 }
